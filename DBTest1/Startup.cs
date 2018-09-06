0290696cc8b1be2d10024e278246a31274bdb7d4
Milos Kis <miloskis12@gmail.com>
2018-09-06 17:18:07 +0200

    Add project files.

---
+++ DBTest1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DBTest1.Startup))]
+namespace DBTest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
